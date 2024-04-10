5125b79a4cfb7f5230549925580aa0fd6682eb70
Pavel Yakovlev <pdyakovlev@mail.ru>
2024-04-10 23:06:23 +0300

    Add /api/profile/events endpoint

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, UserActivities
+from events.models import Event
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
@@ -15,3 +16,28 @@ class UserUpdateSerializer(serializers.ModelSerializer):
             'post',
             'interests'
         ]
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = [
+            'id',
+            'topic',
+            'program',
+            'type_event',
+            'name',
+            'date',
+            'time',
+            'adress',
+            'status',
+            'speaker',
+            'price'
+        ]
+
+
+# Пока сделан только чтоб в базу данные закидывать
+class UserActivitiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserActivities
+        fields = '__all__'
